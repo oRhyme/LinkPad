bb5344803eea63ba71d0c93ade66aa6f6403edd7
oRhyme <ytalha777@gmail.com>
2026-04-20 20:45:18 +0530

    masonry layout and other improvements

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -12,8 +12,8 @@ const Card = ({t,d,i}:{t:string, d:string,i:string}) => {
     <div className="card bg-base-100 card-border z-1 p-3 h-fit min-h-64 w-full break-inside-avoid gap-4! mb-5!">
       <figure className="w-full shrink-0"><img src="mock.jpg" alt="" /></figure>
       <div className="card-body">
-        <textarea maxLength={80} rows={1} style={{ resize: 'none' }} className="card-title h-fit overflow-y-hidden p-2 text-xl! text-base-content!" placeholder="Title" value={title} onChange={(e) => setTitle(e.currentTarget.value)}></textarea>
-        <textarea maxLength={200} rows={1} style={{ resize: 'none' }} className="min-h-5 overflow-y-hidden p-2 mb-7! text-base-content!" placeholder="Description" value={description} onChange={(e) => setDescription(e.currentTarget.value)}></textarea>
+        <textarea maxLength={80} rows={1} style={{ resize: 'none' }} className="card-title h-fit overflow-y-hidden p-2 text-2xl! text-base-content! font-bold font-serif!" placeholder="Title" value={title} onChange={(e) => setTitle(e.currentTarget.value)}></textarea>
+        <textarea maxLength={200} rows={1} style={{ resize: 'none' }} className="min-h-5 overflow-y-hidden p-2 mb-7! text-base-content! text-lg!" placeholder="Description" value={description} onChange={(e) => setDescription(e.currentTarget.value)}></textarea>
         
         <div className="card-actions">
          <img src="delete.svg" alt="" className = "absolute bottom-[4px] right-5 btn size-7 m-0 p-0 bg-transparent"/>
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -15,15 +15,15 @@ const Navbar = () => {
   };
 
   return (
-    <div className="navbar-center bg-base-100 w-full justify-center h-12 mb-5! text-base-content!">
+    <div className="navbar-center bg-base-100 w-full justify-center h-16 mb-5! text-base-content!">
         <div className="flex flex-row items-center justify-start flex-1 gap-1 pl-2 z-2">
         <label htmlFor="side-drawer" className="btn btn-sm btn-ghost bg-base-300 hover:bg-primary p-1 drawer-button">
           <img src="/menu.svg" alt="menu" className="w-8 h-8" />
         </label>
-        <h2 className="text-center font-serif text-3xl">LinkBoard</h2>
+        <h2 className="text-center font-serif text-3xl!">LinkBoard</h2>
         </div>
         <div className="flex flex-1 flex-row justify-end gap-5 items-center pr-3">
-        <UserButton className="bg-base-100 text-primary-content" />
+        <UserButton className="bg-base-100 text-primary-content hover:bg-base-300!" />
         <img 
           src="/moon.svg" 
           className="btn btn-primary rounded-full size-10 p-2 bg-white hover:bg-base-300 cursor-pointer"
