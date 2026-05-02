d2da6c3430e83fd210f2c7b563ea838c18efd358
oRhyme <ytalha777@gmail.com>
2026-05-02 12:04:15 +0530

    UI fixes

--- src/app/components/ListItem.tsx
+++ src/app/components/ListItem.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react'
 const ListItem = ({folder,inAddMode, onEnter} :  {folder : string, inAddMode : boolean,onEnter(value : string) : void}) => {
   const [text,setText] = useState(folder)
   const [isEditing, setIsEditing] = useState(inAddMode)
-  const handleKeyDown = (e : React.KeyboardEvent<HTMLTextAreaElement>)=>{
+  const handleKeyDown = (e : React.KeyboardEvent<HTMLInputElement>)=>{
     if(e.key == "Enter"){
         setIsEditing(false)
         onEnter(text)
@@ -13,7 +13,7 @@ const ListItem = ({folder,inAddMode, onEnter} :  {folder : string, inAddMode : b
   }
   return (
     <li className = "p-1 text-center flex justify-between flex-row">
-        {isEditing ? <textarea maxLength={30} value = {text} autoFocus className = "max-w-[90%] h-6" onBlur = {()=>{setIsEditing(false)}} onKeyDown={handleKeyDown} onChange={(k)=>setText(k.target.value)}/>:<a className = "flex-1 hover:bg-base-300! max-w-[80%]">{text}</a>}
+        {isEditing ? <input type = "text" value = {text} autoFocus className = "max-w-[90%] h-6 overflow-hidden" onBlur = {()=>{setIsEditing(false)}} onKeyDown={handleKeyDown} onChange={(k)=>setText(k.target.value)}/>:<a className = "flex-1 hover:bg-base-300! max-w-[80%] h-6">{text}</a>}
         {!isEditing ? <img src = "/edit.svg" className = "size-4 p-0 m-0 btn btn-primary bg-base-200 block" onClick={()=>{
             setIsEditing(true)
         }}></img> : <p className = "hidden"></p>}
--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -18,7 +18,7 @@ const Sidebar = () => {
     <div className="drawer absolute z-3">
       <input id="side-drawer" type="checkbox" className="drawer-toggle" />
       <div className="drawer-side top-16 h-[calc(100vh-3rem)] w-80">
-        <label htmlFor="side-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+        <label htmlFor="side-drawer" aria-label="close sidebar" className="drawer-overlay w-screen!"></label>
         <ul className=" absolute my-5 menu p-5 w-full bg-base-200 text-base-content min-h-[calc(100vh-3rem)] text-center">
         <div className="flex align-center justify-between my-3!">
           <h2 className = "ml-1 font-bold text-lg font-serif">Folders</h2>
