d4acd2d4c409eaf83034d671d79105fa3678be1e
oRhyme <ytalha777@gmail.com>
2026-06-17 21:42:02 +0530

    shifted add icon from layout to folderpage

--- src/app/folder/page.tsx
+++ src/app/folder/page.tsx
@@ -11,9 +11,12 @@ const page = async ({params} : {params : Promise<{folder : string}>}) => {
       console.log(error)
     }
   return (
+    <>
     <div>This is folder {folder}</div>
+    <img src="/plus.svg" className="btn fixed size-10 bg-green-500 border-0 rounded-full p-0 left-1/2 bottom-5 z-4 -translate-x-1/2 opacity-40 cursor-pointer hover:opacity-100 transition-opacity duration-300 hover:scale-110 transition-scale duration-300" />
+    </>
   )
 
-}
+} 
 
 export default page
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,6 @@ export default function RootLayout({
         <Providers>
           <Navbar />
           <Sidebar />
-          <img src="/plus.svg" className="btn fixed size-10 bg-green-500 border-0 rounded-full p-0 left-1/2 bottom-5 z-4 -translate-x-1/2 opacity-40 cursor-pointer hover:opacity-100 transition-opacity duration-300 hover:scale-110 transition-scale duration-300" />
           {children}
         </Providers>
       </body>
