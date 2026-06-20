aa4df3dbc237ae9e3c6c0fa307e5c90829d3dfd4
oRhyme <ytalha777@gmail.com>
2026-06-20 17:29:38 +0530

    list item UI change

--- src/app/components/ListItem.tsx
+++ src/app/components/ListItem.tsx
@@ -34,6 +34,11 @@ const ListItem = ({
     const{data,error} = await authClient.getSession();
     console.log(data)
   };
+
+  const deleteFolder = async()=>{
+    //
+  }
+
   return (
     <li className="p-1 text-center flex justify-between flex-row" onClick = {itemClicked}>
       {isEditing ? (
@@ -50,19 +55,31 @@ const ListItem = ({
           onChange={(k) => setText(k.target.value)}
         />
       ) : (
-        <a className="flex-1 hover:bg-base-300! max-w-[80%] h-6">{text}</a>
+        <div className="flex flex-row w-80 ">
+          <a className="flex-2 hover:bg-base-300! max-w-[80%] h-6">{text}</a>
+          <img src="/delete.svg" className = "size-4 btn btn-primary bg-base-200" onClick={deleteFolder}/>
+          <img
+            src="/edit.svg"
+            className = "size-4 hover:size-5 btn btn-primary bg-base-200"
+            onClick={() => {
+              setIsEditing(true);
+            } }
+          ></img>
+        </div>
       )}
-      {!isEditing ? (
-        <img
-          src="/edit.svg"
-          className="size-4 p-0 m-0 btn btn-primary bg-base-200 block"
-          onClick={() => {
-            setIsEditing(true);
-          }}
-        ></img>
+      {/* {!isEditing ? (
+        <div className="size-4 p-0 m-0 flex absolute  ">
+          <img src="/delete.svg" />
+          <img
+            src="/edit.svg"
+            onClick={() => {
+              setIsEditing(true);
+            }}
+          ></img>
+        </div>
       ) : (
         <p className="hidden"></p>
-      )}
+      )} */}
       <hr className="w-full opacity-10"></hr>
     </li>
   );
