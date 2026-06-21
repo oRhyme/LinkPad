4f6df6578ee73af1d07f17393536a4ac01ad99fa
oRhyme <ytalha777@gmail.com>
2026-06-21 15:52:59 +0530

    clicking background discards the card

--- src/app/components/NewCard.tsx
+++ src/app/components/NewCard.tsx
@@ -1,14 +1,22 @@
+"use client"
 import React from 'react'
 import Card from './Card'
+import {useState} from 'react'
 const NewCard = () => {
+
+  const [title,setTitle] = useState<string>("")
+  const [description, setDescription] = useState<string>("")
+  const [url, setUrl] = useState<string>("")
+  
+
   return (
     <>
-    <div className = "fixed w-full h-full bg-black top-0 left-0 z-5 opacity-30"></div>
+    
     <div className = "card h-[90vh]! w-[30vw]! p-0! fixed! top-1/2 left-1/2! z-10 -translate-x-1/2 -translate-y-1/2 bg-gray-100">
         <div className = "card-body flex flex-col top-1/12 gap-3 p-2! h-[80%]!">
-            <input type="text" className = "bg-gray-200! w-full h-10! font-bold text-2xl! font-serif! p-2!" placeholder="Title" />
-            <input type="text" className = "bg-gray-200! w-full p-2!" placeholder = "url"/>
-            <textarea maxLength = {200} className = "bg-gray-200! w-full max-h-[60%]! min-h-[60%]!   p-2" placeholder = "Description"></textarea>
+            <input type="text" className = "bg-gray-200! w-full h-10! font-bold text-2xl! font-serif! p-2!" placeholder="Title" value = {title} onChange={(e)=>{setTitle(e.target.value)}}/>
+            <input type="text" className = "bg-gray-200! w-full p-2!" placeholder = "url" value = {url} onChange={(e)=>{setUrl(e.target.value)}}/>
+            <textarea maxLength = {200} className = "bg-gray-200! w-full max-h-[60%]! min-h-[60%]!   p-2" placeholder = "Description" value = {description} onChange={(e)=>{setDescription(e.target.value)}}></textarea>
         </div>
         <div className = "card-actions justify-end p-3! w-full! h-[20%]!">
           <input type="button" className = "btn btn-primary bg-green-400! font-bold w-12! h-6! text-sm text-gray-100! " value="SAVE" />
--- src/app/folder/[folderId]/page.tsx
+++ src/app/folder/[folderId]/page.tsx
@@ -18,7 +18,8 @@ const page = () => {
     <div>This is folder {folderId}</div>
     <img src="/plus.svg" className="btn fixed size-10 bg-green-500 border-0 rounded-full p-0 left-1/2 bottom-5 z-4 -translate-x-1/2 opacity-40 cursor-pointer hover:opacity-100 transition-opacity duration-300 hover:scale-110 transition-scale duration-300" 
     onClick = {handleClick}/>
-    {!hidden && <NewCard/>}
+    {!hidden && <><div className = "fixed w-full h-full bg-black top-0 left-0 z-5 opacity-30"  onClick={()=>{setHidden(true)}}></div>
+    <NewCard/></>}
     </>
   )
 }
