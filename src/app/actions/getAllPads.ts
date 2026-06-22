dc4bafd596604e9c10f14cb575cf2e52a57fc493
oRhyme <ytalha777@gmail.com>
2026-06-22 13:24:39 +0530

    getAllPads action

---
+++ src/app/actions/getAllPads.ts
@@ -0,0 +1,21 @@
+"use server"
+import {prisma} from "../../../lib/prisma"
+
+export const getAllPads = async(FId : number|string)=>{
+
+    try{
+        const pads = await prisma.pad.findMany({
+            where : {
+                folderId: Number(FId)
+            }
+        })
+        return pads
+
+    }
+    catch(error){
+        console.log(error)
+        return null
+    }
+}
+
+    
