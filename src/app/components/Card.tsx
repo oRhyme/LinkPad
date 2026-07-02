e5ae82d3459bcd05bf5d1eb3ca9dd392c3010edb
oRhyme <ytalha777@gmail.com>
2026-07-02 12:24:35 +0530

    finalization

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -18,10 +18,12 @@ const Card = ({t,d,i,c,url,CardId,onDelete}:{t:string, d:string,i:string,c?:stri
     <div className= {`card bg-base-100 card-border z-1 p-3 h-fit min-h-64 break-inside-avoid gap-4! mb-5! w-full ${c}`}>
       <a href={url}> 
       {image?
-      <figure className="size-1/2 shrink-0">
+      <figure className="h-1/2 shrink-0 w-full!">
         <img src={image}/>
-        </figure>:
-        <h1>{url}</h1>}
+        </figure>:<div className = "flex gap-3">
+        <img src={`${url}/favicon.ico`} className = "" alt="" />
+        <h1 className = "text-blue-500">{url}</h1>
+        </div>}
       </a>
       <div className="card-body">
         <textarea rows={1} style={{ resize: 'none' }} className="card-title h-fit overflow-y-hidden p-2 text-2xl! text-base-content! font-bold font-serif!" placeholder="Title" value={title} onChange={(e) => setTitle(e.currentTarget.value)}></textarea>
